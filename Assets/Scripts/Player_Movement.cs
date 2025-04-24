6e43bf9fe29a7055740e8777641e00de885dd3f5
JackahLackah <146274465+JackahLackah@users.noreply.github.com>
2025-04-24 08:05:23 -0500

    Changes to jump

--- Assets/Scripts/Player_Movement.cs
+++ Assets/Scripts/Player_Movement.cs
@@ -12,7 +12,7 @@ public class Player_Movement : MonoBehaviour
     [Header("Horizontal Movement Settings")]
     [SerializeField] private float speed;
     [SerializeField] private float jump;
-    [SerializeField] private float jumpStartTime;
+    [SerializeField] private float jumpHoldFrames;
     [SerializeField] private int maxJumps;
 
 
@@ -24,49 +24,61 @@ public class Player_Movement : MonoBehaviour
     [SerializeField] private LayerMask whatIsWall;
 
 
-    private void Awake() {
+    private void Awake()
+    {
         body = GetComponent<Rigidbody2D>();
+        print("on");
     }
 
-    private void Update() {
+    private void Update()
+    {
         body.linearVelocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.linearVelocity.y);
 
-        if(wallJumpCooldown < 0.2f)
+        //if (wallJumpCooldown < 0.2f)
+
         Jump();
+        Grounded();
     }
-    
+
     void Jump()
     {
-        if (Input.GetKey(KeyCode.W) && Grounded())
+        //reset jump count
+        if (Grounded())
+        {
+            availableJumps = 0;
+        }
+        else if(availableJumps == 0)
+        {
+            availableJumps = 1;
+            print("1");
+        }
+        //initiate jump
+        if (Input.GetKey(KeyCode.W) && availableJumps < maxJumps)
+        {
+            availableJumps++;
+            jumpTime = jumpHoldFrames;
+            print("2");
+        }
+
+        //end jump early
+        //if (!get)
+        {
+            //jumpTime = 0;
+            //print("3");
+        }
+
+        //jump based on the timer
+        if (jumpTime > 0)
         {
             body.linearVelocity = new Vector2(body.linearVelocity.x, jump);
-
-            jumpTime = jumpStartTime;
-            isJumping = true;
-        }
-
-        if (Input.GetKey(KeyCode.W) && isJumping == true)
-        {
-            if (jumpTime > 0)
-            {
-                body.linearVelocity = new Vector2(body.linearVelocity.x, jump);
-                jumpTime -= Time.deltaTime;
-            }
-            else
-            {
-                isJumping = false;
-            }
-        }
-
-        if(Input.GetKeyUp(KeyCode.W))
-        {
-            isJumping = false;
+            jumpTime--;
+            print("4");
         }
     }
 
     public bool Grounded()
     {
-        if(Physics2D.Raycast(groundCheckPoint.position, Vector2.down, groundCheckY, whatIsGround) 
+        if (Physics2D.Raycast(groundCheckPoint.position, Vector2.down, groundCheckY, whatIsGround)
             || Physics2D.Raycast(groundCheckPoint.position + new Vector3(groundCheckX, 0, 0), Vector2.down, groundCheckY, whatIsGround)
             || Physics2D.Raycast(groundCheckPoint.position + new Vector3(-groundCheckX, 0, 0), Vector2.down, groundCheckY, whatIsGround))
         {
@@ -82,6 +94,7 @@ public class Player_Movement : MonoBehaviour
         RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, new Vector2(transform.localScale.x, 0), 0.1f, whatIsWall);
         return raycastHit.collider != null;
     }
+}
 
 
     //OLD TBD CODE
@@ -91,5 +104,37 @@ public class Player_Movement : MonoBehaviour
         {
             body.linearVelocity = new Vector2(body.linearVelocity.x, jump);
         }
-        */
+
+
+
+    if (availableJumps > 0)
+        {
+            if (Input.GetKey(KeyCode.W) && Grounded())
+            {
+                body.linearVelocity = new Vector2(body.linearVelocity.x, jump);
+
+    jumpTime = jumpStartTime;
+                isJumping = true;
+                availableJumps++;
+            }
+
+if (Input.GetKey(KeyCode.W) && isJumping == true)
+{
+    if (jumpTime > 0)
+    {
+        body.linearVelocity = new Vector2(body.linearVelocity.x, jump);
+        jumpTime -= Time.deltaTime;
+    }
+    else
+    {
+        isJumping = false;
+    }
 }
+
+if (Input.GetKeyUp(KeyCode.W))
+{
+    isJumping = false;
+}
+        }
+}
+    */
