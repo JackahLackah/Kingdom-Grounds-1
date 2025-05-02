7daa09302c01726b01907f91cceede05f05cfba6
JackahLackah <146274465+JackahLackah@users.noreply.github.com>
2025-05-02 08:02:19 -0500

    Health and Saw code

---
+++ Assets/Scripts/Health/Health.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] private float startingHealth;
+    public float currentHealth { get; private set; }
+    private Animator anim;
+    private bool dead;
+
+    private void Awake()
+    {
+        currentHealth = startingHealth;
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            TakeDamage(1);
+        }
+    }
+
+    public void TakeDamage(float _damage)
+    {
+        currentHealth = Mathf.Clamp(currentHealth - _damage, 0, startingHealth);
+
+        if(currentHealth > 0)
+        {
+            //hurt
+            anim.SetTrigger("hurt");
+            //add iframes
+        }
+        else
+        {
+            //dead
+            if (!dead)
+            {
+                anim.SetTrigger("die");
+                GetComponent<PlayerNewMovement>().enabled = false;
+                dead = true;
+            }
+        }
+    }
+}
