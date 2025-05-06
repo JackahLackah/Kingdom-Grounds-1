2ef2799b8facfef2d10f60736153d55f4103749f
JackahLackah <146274465+JackahLackah@users.noreply.github.com>
2025-05-06 07:52:59 -0500

    Enemy AI

--- Assets/Scripts/Enemy/LizardEnemy.cs
+++ Assets/Scripts/Enemy/LizardEnemy.cs
@@ -4,10 +4,18 @@ public class LizardEnemy : MonoBehaviour
 {
     [SerializeField] private float attackCooldown;
     [SerializeField] private float range;
+    [SerializeField] private float colliderDistance;
     [SerializeField] private int damage;
     [SerializeField] private BoxCollider2D boxCollider;
     [SerializeField] private LayerMask playerLayer;
     private float cooldownTimer = Mathf.Infinity;
+    private Animator anim;
+    private Health playerHealth;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
 
     private void Update()
     {
@@ -17,21 +25,39 @@ public class LizardEnemy : MonoBehaviour
         {
             if (cooldownTimer >= attackCooldown)
             {
-
+                cooldownTimer = 0;
+                anim.SetTrigger("");
             }
         }
     }
 
     private bool PlayerInSight()
     {
-        RaycastHit2D hit = Physics2D.BoxCast(boxCollider.bounds.center + transform.right * range * transform.localScale.x, boxCollider.bounds.size, 0, Vector2.left, 0, playerLayer);
+        RaycastHit2D hit = Physics2D.BoxCast(boxCollider.bounds.center + transform.right * range * transform.localScale.x * colliderDistance, 
+            new Vector3(boxCollider.bounds.size.x* range, boxCollider.bounds.size.y, boxCollider.bounds.size.z),
+            0, Vector2.left, 0, playerLayer);
+
+        if(hit.collider != null)
+        {
+            playerHealth = hit.transform.GetComponent<Health>();
+        }
+
         return hit.collider != null;
     }
 
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
-        Gizmos.DrawWireCube(boxCollider.bounds.center + transform.right * range * transform.localScale.x, boxCollider.bounds.size);
+        Gizmos.DrawWireCube(boxCollider.bounds.center + transform.right * range * transform.localScale.x * colliderDistance,
+            new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z));
 
+    }
+
+    private void DamagePlayer()
+    {
+        if(PlayerInSight())
+        {
+            playerHealth.TakeDamage(damage);
+        }
     }
 }
